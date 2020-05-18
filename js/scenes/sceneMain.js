66893a8c31ae29ec7096a561fabdceeeca4fb9eb
AlexPullen91 <alex_pullen03@hotmail.com>
2020-05-18 13:49:09 +0000

    Added paddles

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -16,9 +16,20 @@ class SceneMain extends Phaser.Scene {
         // reference to dead center of the game
         this.centerX = game.config.width / 2;
         this.centerY = game.config.height / 2;
+        this.quarter = game.config.height / 4;
 
         this.ball = this.physics.add.sprite(this.centerX, this.centerY, 'balls');
         Align.scaleToGameW(this.ball, .05);
+        //
+        //
+        //
+        this.paddle1 = this.physics.add.sprite(this.centerX, this.quarter, 'paddles');
+        Align.scaleToGameW(this.paddle1, .25)
+        //
+        //
+        //
+        this.paddle2 = this.physics.add.sprite(this.centerX, this.quarter * 3, 'paddles');
+        Align.scaleToGameW(this.paddle2, .25)
 
         //this.alignGrid = new AlignGrid({scene: this, rows: 5, cols: 5});
         //this.alignGrid.showNumbers(); // defines alignGrid class instance
